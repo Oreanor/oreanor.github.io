431c29de99668e74c2562dc40bb3fa04c9f18253
oreanor <oreanor@gmail.com>
2017-09-21 00:51:58 +0300

    Update

--- banners/icepeak/gismeteo/300x600/main.js
+++ banners/icepeak/gismeteo/300x600/main.js
@@ -3,26 +3,31 @@ window.onload = function() {
   var video = document.getElementById("video");
 
   var playButton = document.getElementById("bPlay");
+  var playImg = document.getElementById("play");
   var muteButton = document.getElementById("bSound");
+  var sound1 = document.getElementById("sound1");
+  var sound2 = document.getElementById("sound2");
 
   video.muted = true;
 
   playButton.addEventListener("click", function() {
 	  if (video.paused) {
 	    video.play();
-	    playButton.innerHTML = "Остановить видео";
+	    playImg.style.display = "none";
 	  } else {
 	    video.pause();
-	    playButton.innerHTML = "Воспроизвести видео";
+	    playImg.style.display = "block";
 	  }
 	});
   muteButton.addEventListener("click", function() {
 	  if (!video.muted) {
 	    video.muted = true;
-	    muteButton.innerHTML = "Включить звук";
+	    sound1.style.display = "none";
+	    sound2.style.display = "block";
 	  } else {
 	    video.muted = false;
-	    muteButton.innerHTML = "Выключить звук";
+	    sound1.style.display = "block";
+	    sound2.style.display = "none";
 	  }
 
 	});
