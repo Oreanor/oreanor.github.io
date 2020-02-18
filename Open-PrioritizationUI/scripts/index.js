3e537607a17c25ca690d000f76e320c64d325e7b
Никонец Сергей Владимирович <Nikonets.S.V@sberbank.ru>
2020-02-18 12:16:54 +0300

    Update

--- Open-PrioritizationUI/scripts/index.js
+++
@@ -1,8 +0,0 @@
-import 'babel-polyfill';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-
-VSS.ready(function() {
-	ReactDOM.render(<App />, document.getElementById('root'));
-});
--- Open-PrioritizationUI/scripts/test/index.spec.js
+++
@@ -1,5 +0,0 @@
-describe('Sample Test', () => {
-  test('Using JEST', () => {
-    expect(true).toBe(true);
-  });
-});
